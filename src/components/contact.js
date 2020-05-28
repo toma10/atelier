bc970dfabc9914cb65ba2139f74054634a8c21c9
Tomáš Máčala <tomas.macala@seznam.cz>
2020-05-28 10:06:06 +0200

    Use h5

--- src/components/contact.js
+++ src/components/contact.js
@@ -11,9 +11,9 @@ export default function Contact() {
           <h4 className="text-xl leading-none text-gray-700 sm:text-2xl">
             Oldřich Oškera, DiS.
           </h4>
-          <h6 className="mt-2 leading-none text-gray-600">
+          <h5 className="mt-2 leading-none text-gray-600">
             Horní Lhota u Luhačovic
-          </h6>
+          </h5>
           <h2 className="mt-8 text-2xl leading-none text-gray-600 transition-colors duration-300 sm:text-3xl hover:text-gray-900">
             <a href="mailto:oldrich.oskera@gmail.com">
               oldrich.oskera@gmail.com
